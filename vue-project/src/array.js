10742bcfcfab6af381e0f7ac5661525b3423dd1e
Antongin <antong19@nycstudents.net>
2023-03-20 14:00:21 -0400

    s

---
+++ vue-project/src/array.js
@@ -0,0 +1,33 @@
+const cart = [
+  {
+    id: 1,
+    name: "Hershey",
+    image: "https://images.heb.com/is/image/HEBGrocery/000740766-1",
+  },
+
+  {
+    id: 2,
+    name: "lipton-soup",
+    image:
+      "https://i5.walmartimages.com/asr/0f775896-f961-486f-acfc-baf14377084c_1.14a0791085b77162940f4825efcebdc3.jpeg",
+  },
+  {
+    id: 3,
+    name: "choco-chip cookies",
+    image:
+      "https://i5.walmartimages.com/asr/5902e3f2-960b-4b11-ade6-a77274955633.55663977ef35ff08c3e814b59e6b8d65.jpeg",
+  },
+  {
+    id: 4,
+    name: "chicken",
+    image:
+      "https://cdn.mos.cms.futurecdn.net/BX7vjSt8KMtcBHyisvcSPK-1200-80.jpg",
+  },
+
+  {
+    id: 5,
+    name: "milk",
+    image:
+      "https://images.immediate.co.uk/production/volatile/sites/30/2020/02/Glass-and-bottle-of-milk-fe0997a.jpg",
+  },
+];
