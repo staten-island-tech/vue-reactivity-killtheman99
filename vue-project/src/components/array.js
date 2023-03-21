fc07997dff5334c17a1193befe1101829714540d
Antongin <antong19@nycstudents.net>
2023-03-21 14:00:43 -0400

    s

--- vue-project/src/components/array.js
+++ vue-project/src/components/array.js
@@ -3,6 +3,7 @@ const cart = [
     id: 1,
     name: "Hershey",
     image: "https://images.heb.com/is/image/HEBGrocery/000740766-1",
+    price: 5,
   },
 
   {
@@ -10,18 +11,21 @@ const cart = [
     name: "lipton-soup",
     image:
       "https://i5.walmartimages.com/asr/0f775896-f961-486f-acfc-baf14377084c_1.14a0791085b77162940f4825efcebdc3.jpeg",
+    price: 3,
   },
   {
     id: 3,
     name: "choco-chip cookies",
     image:
       "https://i5.walmartimages.com/asr/5902e3f2-960b-4b11-ade6-a77274955633.55663977ef35ff08c3e814b59e6b8d65.jpeg",
+    price: 6,
   },
   {
     id: 4,
     name: "chicken",
     image:
       "https://cdn.mos.cms.futurecdn.net/BX7vjSt8KMtcBHyisvcSPK-1200-80.jpg",
+    price: 50,
   },
 
   {
@@ -29,5 +33,15 @@ const cart = [
     name: "milk",
     image:
       "https://images.immediate.co.uk/production/volatile/sites/30/2020/02/Glass-and-bottle-of-milk-fe0997a.jpg",
+    price: 2,
+  },
+  {
+    id: 6,
+    name: "cow",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/8/8c/Cow_%28Fleckvieh_breed%29_Oeschinensee_Slaunger_2009-07-07.jpg",
+    price: 200,
   },
 ];
+
+export let c = cart;
